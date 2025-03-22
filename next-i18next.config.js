f5afee1389f7b431691a51f035c27d6677ba973b
nandoll <fantezana@outlook.com>
2025-03-21 23:38:05 -0500

    Lang config

---
+++ next-i18next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-i18next').UserConfig} */
+module.exports = {
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    localeDetection: true,
+  },
+  defaultNS: "common",
+  localePath: "public/locales",
+  reloadOnPrerender: process.env.NODE_ENV === "development",
+};
